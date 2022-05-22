49bc9dd7e281759a99e0279b9775e820235033a3
Abhishek Kanaujiya <65384193+abhi26ak@users.noreply.github.com>
2022-05-22 10:33:27 +0530

    Update q2.cpp

--- Day_1/q2.cpp
+++ Day_1/q2.cpp
@@ -1,7 +1,25 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-int main()
-{
-  return 0;
-}
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int ans = 0;
+        int n = arr.size();
+        for(int i = 0 ; i < arr.size() ;)
+        {
+            int st = i;
+            while(i < n-1 && arr[i] < arr[i+1]) i++;
+            int j = i;
+            while(j != st && i < n-1 && arr[i] > arr[i+1]) i++;
+            if(st != 0 && i != j)
+            {
+                ans = max(ans , i-st+1);
+            }
+            else if(st == 0 && i != j)
+            {
+                ans = max(ans , i-st+1);
+            }
+            if(i == n-1) break;
+            if(st == i) i++;
+        }
+        return ans;
+    }
+};
